0b41ad8e580fa69cfad211c6107d26b7ec530d7d
Ayad <105100677+ayadshkur100221@users.noreply.github.com>
2024-05-02 01:55:43 +0300

    Add files via upload

---
+++ misc/log.js
@@ -0,0 +1,36 @@
+const settings = require('../settings.json')
+const fetch = require('node-fetch')
+
+/**
+ * Log an action to a Discord webhook.
+ * @param {string} action 
+ * @param {string} message 
+ */
+module.exports = (action, message) => {
+    if (!settings.logging.status) return
+    if (!settings.logging.actions.user[action] && !settings.logging.actions.admin[action]) return
+
+    fetch(settings.logging.webhook, {
+        method: 'POST',
+        headers: {
+            'content-type': 'application/json'
+        },
+        body: JSON.stringify({
+            embeds: [
+                {
+                    color: hexToDecimal('#FFFFFF'),
+                    title: `Event: \`${action}\``,
+                    description: message,
+                    author: {
+                        name: 'Flexa Logging'
+                    }
+                }
+            ]
+        })
+    })
+    .catch(() => {})
+}
+
+function hexToDecimal(hex) {
+    return parseInt(hex.replace("#", ""), 16)
+}
